7fcec6406b126e1cc29cb79799e608e01c8962c5
Aymammet <aymammet2018@gmail.com>
2022-02-26 22:09:41 +0500

    28

--- todoAPI/settings/__init__.py
+++ todoAPI/settings/__init__.py
@@ -1,8 +1,8 @@
-from .base import *
+from todoAPI.settings.base import *
 
 environment = os.getenv('DJANGO_SETTINGS_MODULE', 'local')
 
 if environment.endswith('production'):
-    from .production import *
+    from todoAPI.settings.production import *
 else:
-    from .local import *
+    from todoAPI.settings.local import *
