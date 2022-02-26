30f6577ee22022ed9d75f566c2dbbf37e61dc6b9
Aymammet <aymammet2018@gmail.com>
2022-02-26 20:03:36 +0500

    23

--- todoAPI/settings/base.py
+++ todoAPI/settings/base.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
