0def47d15782eb548fcce5bc8ca9d2331e673676
Aymammet <aymammet2018@gmail.com>
2022-02-26 20:12:20 +0500

    24

--- todoAPI/settings/base.py
+++ todoAPI/settings/base.py
@@ -18,9 +18,9 @@ from django.core.exceptions import NON_FIELD_ERRORS
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # BASE_DIR = Path(__file__).resolve().parent.parent.parent
-get_dir = os.path.dirname
-BASE_DIR = get_dir(get_dir(get_dir(os.path.abspath(__file__))))
+# get_dir = os.path.dirname
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
