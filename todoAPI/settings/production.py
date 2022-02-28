6e707e8462c6a94f1b295558a12d132ae907f1aa
Aymammet <aymammet2018@gmail.com>
2022-02-28 22:55:36 +0500

    31

--- todoAPI/settings/production.py
+++ todoAPI/settings/production.py
@@ -2,18 +2,18 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+# DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['https://todo-aymammet.herokuapp.com']
 
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD' : os.getenv('PASSWORD'),
-        'HOST' : os.getenv('HOST'),
-        'PORT' : os.getenv('PORT'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('NAME'),
+#         'USER': os.getenv('USER'),
+#         'PASSWORD' : os.getenv('PASSWORD'),
+#         'HOST' : os.getenv('HOST'),
+#         'PORT' : os.getenv('PORT'),
+#     }
+# }
 
