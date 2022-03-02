c0e2dcac5c11d7856de5c9614d0c5d8e2952cc4d
Aymammet <aymammet2018@gmail.com>
2022-03-02 23:22:41 +0500

    52

--- todoAPI/settings/base.py
+++ todoAPI/settings/base.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'todoAPI.wsgi.application'
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "todo", 
-        'USER': 'postgres',
-        'PASSWORD' : 'postgres',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD' : os.getenv('DB_PASSWORD'),
+        'HOST' : os.getenv('DB_HOST'),
+        'PORT' : os.getenv('DB_PORT'),
     }
 }
 
