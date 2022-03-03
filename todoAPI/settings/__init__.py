582eb79acd37fff000cc670f5f7f5cbc9205c489
Aymammet <aymammet2018@gmail.com>
2022-03-04 00:58:50 +0500

    66

--- todoAPI/settings/__init__.py
+++ todoAPI/settings/__init__.py
@@ -1,6 +1,8 @@
 from todoAPI.settings.base import *
+url = "127.0.0.1"
 
 environment = os.getenv('DJANGO_SETTINGS_MODULE', 'local')
 
 if environment.endswith('production'):
     from todoAPI.settings.production import *
+    url = "https://todo-aymammet.herokuapp.com"
